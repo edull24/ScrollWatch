3abd0a9fc15946067061c199cfc5737aca316f13
edull24 <evandull@gmail.com>
2014-03-15 15:49:47 -0400

    Add initial build process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,4 +2,57 @@ module.exports = function(grunt) {
 
 	'use strict';
 
+	grunt.initConfig({
+
+		srcPath: 'src/',
+		distPath: 'dist/',
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		copy: {
+			all: {
+				files: [
+					{
+						expand: true,
+						cwd: '<%= srcPath %>',
+						src: ['<%= pkg.name %>.js'],
+						dest: '<%= distPath %>'
+
+					}
+				]
+			}
+		},
+
+		uglify: {
+			all: {
+				options: {
+					banner: '/*! <%= pkg.name %> v<%= pkg.version %> | ' +
+							'(c) <%= grunt.template.today("yyyy") %> <%= pkg.author %> | ' +
+							'License: <%= pkg.license %> | ' +
+							'<%= pkg.repository.url %> */',
+					report: 'gzip',
+					sourceMap: true,
+					preserveComments: false,
+					compress: {
+						drop_console: true
+					}
+				},
+				files: {
+					'<%= distPath %><%= pkg.name %>.min.js': ['<%= distPath %><%= pkg.name %>.js']
+				}
+			}
+		}
+
+	});
+
+	// Load grunt tasks from NPM packages.
+	require('load-grunt-tasks')(grunt);
+
+	grunt.registerTask('build', [
+		'copy',
+		'uglify'
+	]);
+
+	grunt.registerTask('default', ['build']);
+
 };
