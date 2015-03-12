9af3709c3c309ee1983063e5356933e071f4bc5c
Evan Dull <evandull@gmail.com>
2015-03-11 23:38:09 -0400

    Reorganize repo for just pages.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,8 +5,8 @@ var plugins = require('gulp-load-plugins')();
 
 gulp.task('styles', function() {
 
-	return gulp.src('./css/app.css')
-		.pipe(plugins.autoprefixer('last 2 versions', 'Explorer > 9'))
+	return gulp.src('./src/css/app.css')
+		.pipe(plugins.autoprefixer('last 2 versions', 'Explorer > 8'))
 		.pipe(gulp.dest('./dist/css'));
 
 });
