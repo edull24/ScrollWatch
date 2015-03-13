d5dd7a6225e13b9541e69e4f702b548043ef729e
Evan Dull <evandull@gmail.com>
2015-03-13 00:01:00 -0400

    UI iteration.

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,25 @@
 // (function() {
 
-	var swHeaderTitle = new window.ScrollWatch({
-		watch: '.header-title',
+	/*var swHeaderTitle = new window.ScrollWatch({
+		watch: '.site-header-title',
 		onElementInView: function(data) {
 			data.el.classList.add('animated', 'lightSpeedIn');
+		}
+	});*/
+
+	var swDefault = new window.ScrollWatch({
+		watch: '.sw-fadeInUp',
+		onElementInView: function(data) {
+			data.el.style.visibility = 'visible';
+			data.el.classList.add('fadeInUp');
 		}
 	});
 
 	var swDefault = new window.ScrollWatch({
-		watch: '.hidden',
+		watch: '.features-list-item',
 		onElementInView: function(data) {
 			data.el.classList.remove('hidden');
-			data.el.classList.add('animated', 'fadeInUp');
+			data.el.classList.add('flipInX');
 		}
 	});
 
