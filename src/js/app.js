1f5722acb060e71bdb3e4c0c651ae58772903b82
Evan Dull <evandull@gmail.com>
2015-03-13 17:27:38 -0400

    Document API.

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,10 @@
 // (function() {
 
-	/*var swHeaderTitle = new window.ScrollWatch({
-		watch: '.site-header-title',
-		onElementInView: function(data) {
-			data.el.classList.add('animated', 'lightSpeedIn');
-		}
-	});*/
-
 	var swDefault = new window.ScrollWatch({
 		watch: '.sw-fadeInUp',
 		onElementInView: function(data) {
-			data.el.style.visibility = 'visible';
+			// console.log(data);
+			data.el.classList.remove('sw-fadeInUp');
 			data.el.classList.add('fadeInUp');
 		}
 	});
@@ -18,6 +12,7 @@
 	var swDefault = new window.ScrollWatch({
 		watch: '.features-list-item',
 		onElementInView: function(data) {
+			console.log(data);
 			data.el.classList.remove('hidden');
 			data.el.classList.add('flipInX');
 		}
