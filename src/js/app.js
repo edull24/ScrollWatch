f1745116decbecb72d58f0e5595215bddab965d2
Evan Dull <evandull@gmail.com>
2015-03-18 16:10:43 -0400

    Update social UI. Add Facebook open graph data.

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,80 @@
 (function() {
 
+	var dom = {};
+
 	var unHide = function(el) {
 
 		el.classList.remove('hidden');
 
 	};
 
-	// Elements to fadeInUP.
-	new window.ScrollWatch({
-		watch: '.sw-fadeInUp',
-		onElementInView: function(data) {
-			data.el.classList.remove('sw-fadeInUp');
-			data.el.classList.add('fadeInUp');
-		}
-	});
+	var setupScrollWatch = function() {
 
-	// Elements to fadeIn.
-	new window.ScrollWatch({
-		watch: '.sw-fadeIn',
-		onElementInView: function(data) {
-			data.el.classList.remove('sw-fadeIn');
-			data.el.classList.add('fadeIn');
-		}
-	});
+		// Elements to fadeInUP.
+		new window.ScrollWatch({
+			watch: '.sw-fadeInUp',
+			onElementInView: function(data) {
+				data.el.classList.remove('sw-fadeInUp');
+				data.el.classList.add('fadeInUp');
+			}
+		});
 
-	// Flip feature cards.
-	new window.ScrollWatch({
-		watch: '.feature-list__item',
-		onElementInView: function(data) {
-			unHide(data.el);
-			data.el.classList.add('flipInX');
-		}
-	});
+		// Elements to fadeIn.
+		new window.ScrollWatch({
+			watch: '.sw-fadeIn',
+			onElementInView: function(data) {
+				data.el.classList.remove('sw-fadeIn');
+				data.el.classList.add('fadeIn');
+			}
+		});
 
-	// Header tagline.
-	new window.ScrollWatch({
-		watch: '.site-header__tagline',
-		onElementInView: function(data) {
-			unHide(data.el);
-			data.el.classList.add('rubberBand');
-		}
-	});
+		// Flip feature cards.
+		new window.ScrollWatch({
+			watch: '.feature-list__item',
+			onElementInView: function(data) {
+				unHide(data.el);
+				data.el.classList.add('flipInX');
+			}
+		});
+
+		// Header tagline.
+		new window.ScrollWatch({
+			watch: '.site-header__tagline',
+			onElementInView: function(data) {
+				unHide(data.el);
+				data.el.classList.add('rubberBand');
+			}
+		});
+
+	};
+
+	var setupDom = function() {
+
+		dom.connectShareBtn = document.querySelector('.site-header__connect-share-btn');
+		dom.socialHeader = document.getElementById('socialHeader');
+
+	};
+
+	var addEventHandlers = function() {
+
+		dom.connectShareBtn.addEventListener('click', toggleConnectShare, false);
+
+	};
+
+	var toggleConnectShare = function(e) {
+
+		dom.socialHeader.classList.toggle('is-expanded');
+
+	};
+
+	var init = function() {
+
+		setupScrollWatch();
+		setupDom();
+		addEventHandlers();
+
+	};
+
+	init();
 
 })();
