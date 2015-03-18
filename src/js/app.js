789798fb64d13fead825167762f0e01182166c73
Evan Dull <evandull@gmail.com>
2015-03-18 17:30:17 -0400

    Wait to inject social js to keep animations jank free.

--- src/js/app.js
+++ src/js/app.js
@@ -67,11 +67,33 @@
 
 	};
 
+	var injectSocialJs = function() {
+
+		// Wait 1s to inject so css animations don't get janky.
+		window.setTimeout(function() {
+
+			// Load facebook js.
+			!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+			// Load twitter js.
+			(function(d, s, id) {
+			  var js, fjs = d.getElementsByTagName(s)[0];
+			  if (d.getElementById(id)) return;
+			  js = d.createElement(s); js.id = id;
+			  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+			  fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+
+		}, 1000);
+
+	};
+
 	var init = function() {
 
 		setupScrollWatch();
 		setupDom();
 		addEventHandlers();
+		injectSocialJs();
 
 	};
 
