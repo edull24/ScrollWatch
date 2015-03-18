e5c1785d1a6917cbce08875a46136591f90a5ace
Evan Dull <evandull@gmail.com>
2015-03-17 23:43:04 -0400

    Add sass.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,8 +5,14 @@ var plugins = require('gulp-load-plugins')();
 
 gulp.task('styles', function() {
 
-	return gulp.src('./src/css/app.css')
+	return gulp.src('./src/css/*.scss')
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.sass({
+			errLogToConsole: true,
+			outputStyle: 'compressed'
+		}))
 		.pipe(plugins.autoprefixer('last 2 versions', 'Explorer > 8'))
+		.pipe(plugins.sourcemaps.write('./'))
 		.pipe(gulp.dest('./dist/css'));
 
 });
