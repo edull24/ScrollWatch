eeba5ebb0801b7169cdc5d32e2b19f507863c370
Evan Dull <evandull@gmail.com>
2015-03-31 00:20:25 -0400

    Add google analytics script.

--- src/js/app.js
+++ src/js/app.js
@@ -60,27 +60,9 @@
 
 	};
 
-	var injectSocialJs = function() {
-
-		// Wait 1s to inject so css animations don't get janky.
-		window.setTimeout(function() {
-
-			/*(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-			ga('create', 'UA-60956379-1', 'auto');
-			ga('send', 'pageview');*/
-
-		}, 1500);
-
-	};
-
 	var init = function() {
 
 		setupScrollWatch();
-		injectSocialJs();
 
 		document.addEventListener('click', function(e) {
 
